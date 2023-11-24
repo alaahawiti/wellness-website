513dc95682bb82e11902f6a173c9d81d67060e9f
alaahawiti <150966309+alaahawiti@users.noreply.github.com>
2023-11-24 13:21:28 +0300

    Add files via upload

--- scriptweb.js
+++ scriptweb.js
@@ -14,3 +14,15 @@ document.form1.text1.focus();
 return false;
 }
 }
+
+
+window.onload = function() {
+    shuffleList();
+};
+
+function shuffleList() {
+    var ul = document.getElementById('randomname');
+    for (var i = ul.children.length; i >= 0; i--) {
+        ul.appendChild(ul.children[Math.random() * i | 0]);
+    }
+}
